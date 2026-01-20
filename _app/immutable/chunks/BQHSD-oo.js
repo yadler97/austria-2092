221a0ff71c5b666b2ebd175e3e2d5e91b8529773
Yannick Adler <yannick.adler@yahoo.de>
2026-01-20 23:19:24 +0100

    new version

--- _app/immutable/chunks/BQHSD-oo.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_e7ornh?.base??"/austria-2092",a=globalThis.__sveltekit_e7ornh?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.BpFGYG-n.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/2q8y9bBO.js";export{o as load_css,r as start};
