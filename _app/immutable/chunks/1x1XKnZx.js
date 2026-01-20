8a5a78892e480ba227f0d5a1636901455b28c64d
Yannick Adler <yannick.adler@yahoo.de>
2026-01-20 23:57:09 +0100

    new version

--- _app/immutable/chunks/1x1XKnZx.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_128srq2?.base??"",a=globalThis.__sveltekit_128srq2?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DuyNG6k8.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ae4knu?.base??"/austria-2092",a=globalThis.__sveltekit_ae4knu?.assets??s??"";export{a,s as b};
